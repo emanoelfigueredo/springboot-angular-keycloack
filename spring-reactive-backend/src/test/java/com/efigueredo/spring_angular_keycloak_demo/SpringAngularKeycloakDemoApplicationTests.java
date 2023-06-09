8a31a6f4260bcde5dfe73987bd4be0bc7b89484e
efigueredo <emanoel.figueredo.1@gmail.com>
2023-06-09 12:39:06 -0300

    backend adicionado. Utilizando spring reactive

---
+++ spring-reactive-backend/src/test/java/com/efigueredo/spring_angular_keycloak_demo/SpringAngularKeycloakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efigueredo.spring_angular_keycloak_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularKeycloakDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
